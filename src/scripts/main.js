631262f3205cb875bb552659be98f142cbd17a34
Mykola <nyr3mvneov@gmail.com>
2025-08-27 19:07:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,19 @@
 
 const notification = document.querySelector('[data-qa="notification"]');
 
+if (!notification) {
+  document.createElement('div');
+  notification.setAttribute('data-qa', 'notification');
+  document.body.appendChild(notification);
+}
+
 const firstPromise = new Promise((resolve, reject) => {
   const timer = setTimeout(() => {
-    reject('First promise was rejected');
+    reject(new Error('First promise was rejected'));
   }, 3000);
 
-  function onClick(event) {
-    if (event.button === 0) {
+  function onClick(ev) {
+    if (ev.button === 0) {
       clearTimeout(timer);
       resolve('First promise was resolved');
       document.removeEventListener('click', onClick);
@@ -19,8 +25,8 @@ const firstPromise = new Promise((resolve, reject) => {
 });
 
 const secondPromise = new Promise((resolve) => {
-  function onClick(event) {
-    if (event.button === 0 || event.button === 2) {
+  function onClick(ev) {
+    if (ev.button === 0 || ev.button === 2) {
       resolve('Second promise was resolved');
       document.removeEventListener('click', onClick);
     }
@@ -32,9 +38,14 @@ const thirdPromise = new Promise((resolve) => {
   let leftClick = false;
   let rightClick = false;
 
-  function onClick(event) {
-    if (event.button === 0) leftClick = true;
-    if (event.button === 2) rightClick = true;
+  function onClick(ev) {
+    if (ev.button === 0) {
+      leftClick = true;
+    }
+
+    if (ev.button === 2) {
+      rightClick = true;
+    }
 
     if (leftClick && rightClick) {
       resolve('Third promise was resolved');
